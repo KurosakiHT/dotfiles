3de4268557f1e45ee582372776461265a90dfd8a
Vu Ngoc Thinh <thinhbn69@gmail.com>
2021-11-11 10:12:42 +0700

    Initial commit

---
+++ .config/nvim/init.vim
@@ -0,0 +1,48 @@
+:set number
+:set relativenumber
+:set autoindent
+:set tabstop=4
+:set shiftwidth=4
+:set smarttab
+:set softtabstop=4
+:set mouse=a
+
+lua require('plugins')
+lua << END
+require'nvim-treesitter.configs'.setup {
+  matchup = {
+    enable = true,              -- mandatory, false will disable the whole extension
+  },
+}
+
+require'lualine'.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'material',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff',
+                  {'diagnostics', sources={'nvim_lsp', 'coc'}}},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+}
+require'lspconfig'.ccls.setup{}
+END
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,51 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Simple plugins can be specified as strings
+  use '9mm/vim-closer'
+
+  -- Smart completion and syntax check plus LSP
+  use {'neoclide/coc.nvim', branch = 'release'}
+
+  -- Post-install/update hook with neovim command
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+  -- Post-install/update hook with call of vimscript function with argument
+  use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
+
+  -- Multiples icon for various plugins
+  use 'kyazdani42/nvim-web-devicons'
+
+  -- Custom statusline
+  use {
+  'nvim-lualine/lualine.nvim',
+  requires = {'kyazdani42/nvim-web-devicons', opt = true}
+}
+
+  --Matchup command
+  use 'andymass/vim-matchup'
+
+  --Syntax highlighting with vim-matchup intergrated
+  use 'nvim-treestter/nvim-treesitter'
+
+  --File system explorer
+  use 'preservim/nerdtree'
+
+  --Intergrated terminal
+  use 'tc50cal/vim-terminal'
+
+  --Config for LSP
+  use 'neovim/nvim-lspconfig'
+
+  --Better tab bar
+  use {
+  'romgrk/barbar.nvim',
+  requires = {'kyazdani42/nvim-web-devicons'}
+}
+
+  --Syntax highlighting for multiplies languages
+  use 'sheerun/vim-polyglot'
+end)
